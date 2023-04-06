a36df3441328e5ec841ba81b9c541d4ecb79d5ae
AVaraksina <AVaraksina@mlg.ru>
2023-04-06 21:59:12 +0300

    Reset project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServersComponent } from './servers/servers.component';
-import { ServerComponent } from './servers/server/server.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServersComponent,
-    ServerComponent
   ],
   imports: [
     BrowserModule,
