34ba9f5da6de127605c8931acf550ae9aedd8aa0
AVaraksina <AVaraksina@mlg.ru>
2023-04-07 14:46:22 +0300

    recipes template and databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.less']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'Test desc', 'https://img.delo-vcusa.ru/2017/05/Blyuda-iz-molodoy-kartoshki-TOP-6-retseptov.jpg'),
+    new Recipe('Test recipe 2', 'Test desc 2', 'https://img.delo-vcusa.ru/2017/05/Blyuda-iz-molodoy-kartoshki-TOP-6-retseptov.jpg')
+  ];
 
 }
