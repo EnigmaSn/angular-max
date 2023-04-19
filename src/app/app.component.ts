aff269e965a9aebe4c258dee80c46710138e60e9
Anna Varaksina <enigmasnape@gmail.com>
2023-04-19 14:02:03 +0300

    start directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+  // numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
+  onlyOdd = false;
 }
