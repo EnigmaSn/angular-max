1b5cb3c1b5a2e9edbee3a0778512e21e7c2ff763
Anna Varaksina <enigmasnape@gmail.com>
2023-04-19 12:25:34 +0300

    add ingredient

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Output, ViewChild, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.less']
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() addedItem = new EventEmitter<Ingredient>();
 
+  onAddItem() {
+    const ingName: string = this.nameInputRef.nativeElement.value;
+    const amountName: number = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, amountName)
+    this.addedItem.emit(newIngredient);
+  }
 }
