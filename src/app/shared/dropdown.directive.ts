d022e6a53471b18742fcaa1ebc4587eb49f35e43
Anna Varaksina <enigmasnape@gmail.com>
2023-04-24 16:02:12 +0300

    dropdown directive added outside click

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -12,13 +12,31 @@ export class DropdownDirective {
     private elRef: ElementRef
     ) { }
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  // @HostListener('click') toggleOpen() {
+  //   this.isOpen = !this.isOpen;
+  //   const menu = this.elRef.nativeElement.children[1];
+
+  //   if (this.isOpen) {
+  //     this.renderer.addClass(menu, 'show');
+  //   } else {
+  //     this.renderer.removeClass(menu, 'show');
+  //   }
+  // }
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
     const menu = this.elRef.nativeElement.children[1];
 
-    if (this.isOpen) {
-      this.renderer.addClass(menu, 'show');
+    if (this.elRef.nativeElement.contains(event.target)) {
+      this.isOpen = !this.isOpen;
+      const menu = this.elRef.nativeElement.children[1];
+
+      if (this.isOpen) {
+        this.renderer.addClass(menu, 'show');
+      } else {
+        this.renderer.removeClass(menu, 'show');
+      }
+
     } else {
+      this.isOpen = false;
       this.renderer.removeClass(menu, 'show');
     }
   }
