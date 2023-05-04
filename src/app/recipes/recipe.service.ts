34095a823f6014fa620b1b7914e2d1227a987c54
Anna Varaksina <enigmasnape@gmail.com>
2023-05-04 16:53:37 +0300

    shopping-list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,29 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('Test recipe', 'Test desc', 'https://img.delo-vcusa.ru/2017/05/Blyuda-iz-molodoy-kartoshki-TOP-6-retseptov.jpg'),
-    new Recipe('Test recipe 2', 'Test desc 2', 'https://img.delo-vcusa.ru/2017/05/Blyuda-iz-molodoy-kartoshki-TOP-6-retseptov.jpg')
+    new Recipe(
+      'Burger', 
+      'Delicious burger', 
+      'https://img.delo-vcusa.ru/2017/05/Blyuda-iz-molodoy-kartoshki-TOP-6-retseptov.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Bread', 2),
+        new Ingredient('Onion', 0.5),
+      ]),
+    new Recipe(
+      'Test recipe 2', 
+      'Test desc 2', 
+      'https://img.delo-vcusa.ru/2017/05/Blyuda-iz-molodoy-kartoshki-TOP-6-retseptov.jpg',
+      [
+        new Ingredient('Meat', 10),
+        new Ingredient('Bread', 5),
+        new Ingredient('Onion', 7),
+      ]),
   ];
   
   constructor() { }
