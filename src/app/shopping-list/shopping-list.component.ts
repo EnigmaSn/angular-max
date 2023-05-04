d0c7d6ea49f4e6bd7481733cf0d3c6b663dbae9f
Anna Varaksina <enigmasnape@gmail.com>
2023-05-04 16:54:06 +0300

    shopping-list service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
-import { disableDebugTools } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-shopping-list',
